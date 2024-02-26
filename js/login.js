a54d54d5faa493d0f144c854448aab8aa594df23
mdshakil04 <m.shakil04@yahoo.com>
2024-02-26 10:55:56 +0600

    Login credintials enable

---
+++ js/login.js
@@ -0,0 +1,16 @@
+console.log("Welcome to the server")
+document.getElementById('btn-submit').addEventListener('click', function(){
+    // console.log('Button Clicked')
+    const emailField = document.getElementById('email-field');
+    const email = emailField.value;
+    // console.log(email)
+    const passwordField = document.getElementById('password-field');
+    const password = passwordField.value;
+    // console.log(email, password)
+    if(email === 'shakil@gmail.com' && password === '112233'){
+        window.location.href = '/main.html';
+    }else{
+        alert('Invalid credentials');
+    }
+
+})
