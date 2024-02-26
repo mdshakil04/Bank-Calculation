d1b35a5f72e7178f7f36a5c7a5f670a89e7742d9
mdshakil04 <m.shakil04@yahoo.com>
2024-02-27 01:14:39 +0600

    Withdrawl calculation added

--- js/functionality.js
+++ js/functionality.js
@@ -14,8 +14,29 @@ document.getElementById("btn-deposit").addEventListener("click", function(){
     const totalBalanceElement = document.getElementById('total-balance');
     const totalBalanceString = totalBalanceElement.innerText;
     const totalBalance = parseFloat(totalBalanceString);
+
     const newBalance = totalBalance + depositAmount;
     totalBalanceElement.innerText = newBalance;
 
     depositField.value = '';
+   
 })
+ // -----------------Withdrawal functionality ----------------
+ document.getElementById('btn-withdraw').addEventListener('click', function(){
+    // console.log('Withdraw button clicked')
+    const widthdrawField = document.getElementById('withdraw-field');
+    // console.log(widthdrawField)
+    const withdrawFieldValueString = widthdrawField.value;
+    // console.log(withdrawFieldValueString)
+    const withdrawFieldValue = parseFloat(withdrawFieldValueString);
+    const withdrawElement = document.getElementById('withdraw-total');
+    // console.log(withdrawElement)
+    const withdrawElementString = withdrawElement.innerText;
+    // console.log(withdrawElementString)
+    const withdrawAmount = parseFloat(withdrawElementString);
+    const totalWithdraw = withdrawFieldValue + withdrawAmount;
+    console.log(totalWithdraw)
+    withdrawElement.innerText = totalWithdraw;
+    // -------------total Calculations---------------------
+    widthdrawField.value = '';
+ })
