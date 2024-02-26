d9b070ffc04386f4cff47515c1df5eefb3885c12
mdshakil04 <m.shakil04@yahoo.com>
2024-02-27 00:48:01 +0600

    Deposit string problem solved

--- js/functionality.js
+++ js/functionality.js
@@ -1,8 +1,15 @@
 document.getElementById("btn-deposit").addEventListener("click", function(){
     // console.log('btn clicked')
     const depositField = document.getElementById("deposit-field");
-    const depositAmount = depositField.value;
+    const depositAmountString = depositField.value;
+    const depositAmount = parseFloat(depositAmountString)
     // console.log(deposit)
     const depositElement = document.getElementById('deposit-element');
-    depositElement.innerText = depositAmount;
+    const previousDepositString = depositElement.innerText;
+    const previousDeposit = parseFloat(previousDepositString);
+
+    const totalDeposit = previousDeposit + depositAmount;
+    depositElement.innerText = totalDeposit;
+
+    depositField.value = '';
 })
